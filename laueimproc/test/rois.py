198c4392689f3a210ba4f55f4727219714b2786e
robin richard <serveurpython.oz@gmail.com>
2024-04-03 11:36:39 +0200

    debug C memory count ref segfault

--- laueimproc/test/rois.py
+++ laueimproc/test/rois.py
@@ -6,8 +6,10 @@
 import gc
 import tracemalloc
 
+import numpy as np
 import torch
 
+from laueimproc.opti.rois import filter_by_indexs
 from laueimproc.opti.rois import imgbboxes2raw
 
 
@@ -18,20 +20,46 @@ def test_memory_imgbboxes2raw():
     bboxes[::2, 2], bboxes[1::2, 2], bboxes[::2, 3], bboxes[1::2, 3] = 10, 20, 30, 40
 
     tracemalloc.start()
-    gc.collect()
-    snapshot1 = tracemalloc.take_snapshot()
-    for _ in range(10):
-        img_copy, bboxes_copy = img.clone(), bboxes.clone()
-        imgbboxes2raw(img_copy, bboxes_copy).copy()  # copy to be shure countref >= 1
-        img_copy.clone(), bboxes_copy.clone()  # to be shure countref >= 1
+    for i in range(100):
+        img_cpy, bboxes_cpy = img.clone(), bboxes.clone()
+        imgbboxes2raw(img_cpy, bboxes_cpy).hex()  # copy to be shure countref >= 1
+        (img_cpy, bboxes_cpy) = img_cpy.sum(), bboxes_cpy.sum()  # to be shure countref >= 1
+        if i == 0:
+            gc.collect()
+            snapshot1 = tracemalloc.take_snapshot()
     gc.collect()
     snapshot2 = tracemalloc.take_snapshot()
     tracemalloc.stop()
     top_stat = snapshot2.compare_to(snapshot1, "lineno")[0]
-    # print(top_stat.size_diff)
+    print(top_stat.size_diff)
     assert top_stat.size_diff < 1_000, \
         f"imgbboxes2raw memory leak of {top_stat.size_diff} bytes"
 
 
+def test_memory_filter_by_indexs():
+    """Test imgbboxes2raw memory leak."""
+    indexs = torch.tensor([1, 1, 0, -1, -2, *range(100, 1000)])
+    bboxes = torch.zeros((1000, 4), dtype=torch.int32)
+    bboxes[::2, 2], bboxes[1::2, 2], bboxes[::2, 3], bboxes[1::2, 3] = 10, 20, 30, 40
+    data = bytearray(np.linspace(0, 1, (bboxes[:, 2]*bboxes[:, 3]).sum(), dtype=np.float32).tobytes())
+
+    tracemalloc.start()
+    for i in range(100):
+        indexs_cpy, data_cpy, bboxes_cpy = indexs.clone(), data.copy(), bboxes.clone()
+        new_data, new_bboxes = filter_by_indexs(indexs_cpy, data_cpy, bboxes_cpy)
+        new_dat = new_data.hex(), new_bboxes.sum()  # to be shure countref >= 1
+        indexs_cpy, data_cpy, bboxes_cpy = indexs_cpy.sum(), data_cpy.hex(), bboxes_cpy.sum()  # to be shure countref >= 1
+        if i == 0:
+            gc.collect()
+            snapshot1 = tracemalloc.take_snapshot()
+    gc.collect()
+    snapshot2 = tracemalloc.take_snapshot()
+    tracemalloc.stop()
+    top_stat = snapshot2.compare_to(snapshot1, "lineno")[0]
+    print(top_stat.size_diff)
+    assert top_stat.size_diff < 1_000, \
+        f"memory_filter_by_indexs memory leak of {top_stat.size_diff} bytes"
+
 if __name__ == "__main__":
     test_memory_imgbboxes2raw()
+    test_memory_filter_by_indexs()
