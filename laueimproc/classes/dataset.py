aff5c437e1af3d02f079a7da8691b671af410978
robin richard <serveurpython.oz@gmail.com>
2024-04-10 12:50:10 +0200

    manage diagram in a plan

--- laueimproc/classes/dataset.py
+++ laueimproc/classes/dataset.py
@@ -15,6 +15,7 @@ import traceback
 import typing
 import warnings
 
+import torch
 import tqdm
 
 from .diagram import Diagram
@@ -77,13 +78,15 @@ class DiagramDataset(threading.Thread):
 
     Attributes
     ----------
-    diagrams : list[Diagram or None]
-        The full list of the diagram such as self.diagrams[idx] == self[idx].
-        If a diagram is not in the dataset, None is return.
+    diagrams : list[Diagram]
+        The full list of the diagrams. If a diagram is not in the dataset, it is squeeze.
     """
 
     def __init__(
-        self, *diagram_refs, diag2ind: typing.Optional[typing.Callable[[Diagram], int]] = None
+        self,
+        *diagram_refs,
+        diag2ind: typing.Optional[typing.Callable[[Diagram], numbers.Integral]] = None,
+        position: typing.Optional[typing.Callable[[int], tuple[numbers.Real, numbers.Real]]] = None,
     ):
         """Initialise the dataset.
 
@@ -96,36 +99,20 @@ class DiagramDataset(threading.Thread):
             If provided, this function has to be pickalable.
             It has to take only one positional argument (a simple Diagram instance)
             and to return a positive integer.
+        position : callable, optional
+            Provides information on the phisical position of the images on the sample.
+            This function takes as input the index of the diagram,
+            and returns the 2 coordinates in the sample plane.
         """
         if diag2ind is None:
             self._diag2ind = default_diag2ind
         else:
-            assert callable(diag2ind), \
-                f"`diag2ind` has to be callable, not {diag2ind.__class__.__name__}"
-            try:
-                pickle.dumps(diag2ind)
-            except pickle.PicklingError as err:
-                raise AssertionError(
-                    f"the function `diag2ind` {diag2ind} is not pickalable"
-                ) from err
-            signature = inspect.signature(diag2ind)
-            assert len(signature.parameters) == 1, "the function `diag2ind` has to take 1 parameter"
-            parameter = next(iter(signature.parameters.values()))
-            assert parameter.kind in {parameter.POSITIONAL_ONLY, parameter.POSITIONAL_OR_KEYWORD}
-            if parameter.annotation is parameter.empty:
-                warnings.warn("please specify the input type of `diag2ind`", SyntaxWarning)
-            elif parameter.annotation is not Diagram:
-                raise AssertionError(
-                    f"the function `diag2ind` has to get a Diagram, not {parameter.annotation}"
-                )
-            if signature.return_annotation is parameter.empty:
-                warnings.warn("please specify the return type of `diag2ind`", SyntaxWarning)
-            elif signature.return_annotation is not int:
-                raise AssertionError(
-                    f"the function `diag2ind` has to return int, not {signature.return_annotation}"
-                )
+            self._check_diag2ind(diag2ind)
             self._diag2ind = diag2ind
-        self._diagrams: dict[int, Diagram] = {}  # index: diagram
+        if position is not None:
+            self._check_position(position)
+        self._position = [position, {}]  # the dict associate (x, y) to index
+        self._diagrams: dict[int, Diagram] = {}  # index to diagram
         self._lock = threading.Lock()
         self._to_sniff: dict = {"dirs": [], "readed": set()}  # for the async run method
         self._operations_chain: list[typing.Callable[[Diagram], object]] = []
@@ -145,7 +132,7 @@ class DiagramDataset(threading.Thread):
         -------
         Diagram or Dataset
             If the item is integer, return the corresponding diagram,
-            return a frozen dataset view if it is a slice.
+            return a frozen sub dataset view if it is a slice.
 
         Raises
         ------
@@ -153,34 +140,138 @@ class DiagramDataset(threading.Thread):
             If the diagram of index `item` is not founded yet.
         """
         if isinstance(item, numbers.Integral):
-            assert item >= 0, f"only positive indexs are allowed, not {item}"
-            item = int(item)
-            with self._lock:
-                try:
-                    diagram = self._diagrams[item]
-                except KeyError as err:
-                    raise IndexError(f"The diagram of index {item} is not in the dataset") from err
-            if not isinstance(diagram, Diagram):
-                raise IndexError(f"The diagram of index {item} is coming soon in the dataset")
-            return diagram
+            return self._get_diagram_from_index(item)
         if isinstance(item, slice):
-            with self._lock:
-                id_max = max(self._diagrams)
-                item = range(*item.indices(id_max+1))
-                try:
-                    diagrams = {i: self._diagrams[i] for i in item}
-                except KeyError as err:
-                    raise IndexError("one of the asked diagram is not in the dataset") from err
-                new_dataset = DiagramDataset()
-                new_dataset._diag2ind = self._diag2ind
-                new_dataset._diagrams = diagrams
-                new_dataset._operations_chain = self._operations_chain
-                return new_dataset
-        raise TypeError(f"only int and sclices arre allowed, not {item.__class__.__name__}")
+            return self._get_diagrams_from_slice(item)
+        if isinstance(item, tuple):  # case grid
+            if self._position[0] is None:
+                raise AttributeError(
+                    "you must provide the `position` argument to acces the position"
+                )
+            assert len(item) == 2, f"only 2d position allow, not {len(item)}d"
+            if isinstance(item[0], numbers.Real) and isinstance(item[1], numbers.Real):
+                return self._get_diagram_from_coord(*item)
+
+        raise TypeError(f"only int, slices and tuple[float, float] are allowed, not {item}")
 
     def __len__(self) -> int:
         """Return the approximative numbers of diagrams soon be present in the dataset."""
-        return len(self._diagrams)
+        return max(self._diagrams) + 1
+
+    @staticmethod
+    def _check_diag2ind(diag2ind: typing.Callable[[Diagram], numbers.Integral]):
+        """Ensure that the indexing function has right input / output."""
+        assert callable(diag2ind), \
+            f"`diag2ind` has to be callable, not {diag2ind.__class__.__name__}"
+        try:
+            pickle.dumps(diag2ind)
+        except pickle.PicklingError as err:
+            raise AssertionError(f"the function `diag2ind` {diag2ind} is not picklable") from err
+        signature = inspect.signature(diag2ind)
+        assert len(signature.parameters) == 1, "the function `diag2ind` has to take 1 parameter"
+        parameter = next(iter(signature.parameters.values()))
+        assert parameter.kind in {parameter.POSITIONAL_ONLY, parameter.POSITIONAL_OR_KEYWORD}
+        if parameter.annotation is parameter.empty:
+            warnings.warn("please specify the input type of `diag2ind`", SyntaxWarning)
+        elif parameter.annotation is not Diagram:
+            raise AssertionError(
+                f"the function `diag2ind` has to get a Diagram, not {parameter.annotation}"
+            )
+        if signature.return_annotation is parameter.empty:
+            warnings.warn("please specify the return type of `diag2ind`", SyntaxWarning)
+        elif not issubclass(signature.return_annotation, numbers.Integral):
+            raise AssertionError(
+                f"the function `diag2ind` has to return int, not {signature.return_annotation}"
+            )
+
+    @staticmethod
+    def _check_position(position: typing.Callable[[int], tuple[numbers.Real, numbers.Real]]):
+        """Ensures that the position function has the right input / outputs."""
+        assert callable(position), \
+            f"`position` has to be callable, not {position.__class__.__name__}"
+        try:
+            pickle.dumps(position)
+        except pickle.PicklingError as err:
+            raise AssertionError(f"the function `position` {position} is not picklable") from err
+        signature = inspect.signature(position)
+        assert len(signature.parameters) == 1, "the function `position` has to take 1 parameter"
+        parameter = next(iter(signature.parameters.values()))
+        if parameter.annotation is parameter.empty:
+            warnings.warn("please specify the input type of `position`", SyntaxWarning)
+        elif parameter.annotation is not int:
+            raise AssertionError(
+                f"the function `position` has to get a int, not {parameter.annotation}"
+            )
+        if signature.return_annotation is parameter.empty:
+            warnings.warn("please specify the return type of `position`", SyntaxWarning)
+        elif (
+            signature.return_annotation is not tuple
+            and typing.get_origin(signature.return_annotation) is not tuple
+        ):
+            raise AssertionError(
+                f"the function `position` has to return tuple, not {signature.return_annotation}"
+            )
+        return_args = typing.get_args(signature.return_annotation)
+        assert len(return_args) == 2, \
+                f"the function `position` has to return 2 elements, not {len(return_args)}"
+        assert issubclass(return_args[0], numbers.Real), \
+            f"first output element of `position` has to be a real number, not a {return_args[0]}"
+        assert issubclass(return_args[1], numbers.Real), \
+            f"second output element of `position` has to be a real number, not a {return_args[1]}"
+
+    def _get_diagram_from_coord(self, first_idx: numbers.Real, second_idx: numbers.Real) -> Diagram:
+        """Return the closest diagram of the given position."""
+        assert isinstance(first_idx, numbers.Real), first_idx.__class__.__name__
+        assert isinstance(second_idx, numbers.Real), second_idx.__class__.__name__
+        with self._lock:
+            if isinstance(self._position[1], dict):  # dict to tensor
+                coords, indexs = zip(*self._position[1].items())
+                self._position[1] = (
+                    torch.tensor(coords, dtype=torch.float32),
+                    torch.tensor(indexs, dtype=int),
+                )
+            coord = torch.tensor(
+                [[first_idx, second_idx]],
+                dtype=self._position[1][0].dtype,
+                device=self._position[1][0].device,
+            )
+            dist = torch.sum((self._position[1][0].unsqueeze(0) - coord.unsqueeze(1))**2, dim=2)
+            pos = torch.argmin(dist, dim=1).item()
+            index = self._position[1][1][pos].item()
+        return self._get_diagram_from_index(index)
+
+    def _get_diagram_from_index(self, index: numbers.Integral) -> Diagram:
+        """Return the diagram of index `index`."""
+        assert isinstance(index, numbers.Integral), index.__class__.__name__
+        if index < 0:
+            index += len(self)
+            assert index >= 0, "the provided index is to negative"
+        index = int(index)
+        with self._lock:
+            try:
+                diagram = self._diagrams[index]
+            except KeyError as err:
+                raise IndexError(f"The diagram of index {index} is not in the dataset") from err
+        if not isinstance(diagram, Diagram):
+            raise IndexError(f"The diagram of index {index} is coming soon in the dataset")
+        return diagram
+
+    def _get_diagrams_from_slice(self, indexs: slice):
+        """Return a frozen sub dataset view."""
+        assert isinstance(indexs, slice), indexs.__class__.__name__
+        with self._lock:
+            id_max = max(self._diagrams)
+            indexs = range(*indexs.indices(id_max+1))
+            try:
+                diagrams = {i: self._diagrams[i] for i in indexs}
+            except KeyError as err:
+                raise IndexError("one of the asked diagram is not in the dataset") from err
+            new_dataset = DiagramDataset()
+            new_dataset._diag2ind = self._diag2ind
+            new_dataset._position = self._position.copy()
+            new_dataset._diagrams = diagrams
+            new_dataset._operations_chain = self._operations_chain
+            return new_dataset
 
     def add_diagram(self, new_diagram: Diagram):
         """Append a new diagram into the dataset.
@@ -196,6 +287,8 @@ class DiagramDataset(threading.Thread):
             If the diagram is already present in the dataset.
         """
         assert isinstance(new_diagram, Diagram), new_diagram.__class__.__name__
+
+        # get and check index
         index = self._diag2ind(new_diagram)
         assert isinstance(index, numbers.Integral), (
             f"the function {self._diag2ind} must return an integer, "
@@ -204,9 +297,33 @@ class DiagramDataset(threading.Thread):
         assert index >= 0, \
             f"the function {self._diag2ind} must return a positive number, not {index}"
         index = int(index)
+
+        # get an check grid position
+        if self._position[0] is not None:
+            coord = self._position[0](index)
+            assert isinstance(coord, tuple), \
+                f"the function {self._position[0]} must return a tuple, not {coord}"
+            assert len(coord) == 2, \
+                f"the function {self._position[0]} must return 2 elements, not {len(coord)}"
+            assert isinstance(coord[0], numbers.Real), \
+                f"the first element returned by {self._position[0]} must be a number not {coord[0]}"
+            assert isinstance(coord[1], numbers.Real), \
+                f"the first element returned by {self._position[1]} must be a number not {coord[1]}"
+            coord = (float(coord[0]), float(coord[1]))
+        else:
+            coord = None
+
+        # add diagram and check unicity
         with self._lock:
             if index in self._diagrams:
                 raise LookupError(f"the diagram of index {index} is already present in the dataset")
+            if coord is not None:
+                if isinstance(self._position[1], tuple):  # tensor to dict
+                    self._position[1] = {
+                        (x, y): i for (x, y), i in
+                        zip(self._position[1][0].tolist(), self._position[1][1].tolist())
+                    }
+                self._position[1][coord] = index
             if self._operations_chain:
                 self._diagrams[index] = queue.Queue()
                 _ChainThread(new_diagram, self._operations_chain, self._diagrams[index]).start()
@@ -226,18 +343,25 @@ class DiagramDataset(threading.Thread):
                 * laueimproc.classes.diagram.Diagram : Could be a simple Diagram instance.
                 * iterable : An iterable of any of the types specified above.
         """
+        gen_type = type((lambda: (yield))())
         if isinstance(new_diagrams, Diagram):
             self.add_diagram(new_diagrams)
         elif isinstance(new_diagrams, pathlib.Path):
             new_diagrams.expanduser().resolve()
             assert new_diagrams.exists(), f"{new_diagrams} if not an existing path"
             if new_diagrams.is_dir():
+                self.add_diagrams(  # optional, no procrastination
+                    (
+                        f for f in new_diagrams.iterdir()
+                        if f.suffix.lower() not in {".jp2", ".mccd", ".tif", ".tiff"}
+                    )
+                )
                 self._to_sniff["dirs"].append([new_diagrams, 0.0])
             else:
                 self.add_diagram(Diagram(new_diagrams))  # case file
         elif isinstance(new_diagrams, (str, bytes)):
             self.add_diagrams(pathlib.Path(new_diagrams))
-        elif isinstance(new_diagrams, (tuple, list, set, frozenset)):
+        elif isinstance(new_diagrams, (tuple, list, set, frozenset, gen_type)):
             for new_diagram in new_diagrams:
                 self.add_diagrams(new_diagram)
         else:
@@ -307,8 +431,7 @@ class DiagramDataset(threading.Thread):
     def diagrams(self) -> list[typing.Union[Diagram, None]]:
         """Return all the diagrams, pad missing by None."""
         with self._lock:
-            id_max = max(self._diagrams)
-            return [self._diagrams.get(i, None) for i in range(id_max+1)]
+            return [d for d in self._diagrams.values() if isinstance(d, Diagram)]
 
     def run(self):
         """Run asynchronousely in a child thread, called by self.start()."""
